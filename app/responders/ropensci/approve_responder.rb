b4258f27e0eeae1ee93b231d8bc654fc1091de9f
Juanjo Bazán <jjbazan@gmail.com>
2021-07-29 13:23:04 +0200

    add complete approve actions

--- app/responders/ropensci/approve_responder.rb
+++ app/responders/ropensci/approve_responder.rb
@@ -15,11 +15,12 @@ module Ropensci
 
       update_or_add_value("date-accepted", Time.now.strftime("%Y-%m-%d"), append: false, heading: "Date accepted")
       respond_external_template(params[:template_file], locals) if params[:template_file]
+      Ropensci::AirtableWorker.perform_async(:clear_assignments, params, locals, { reviewers: list_of_reviewers })
       process_labeling
       close_issue
     end
 
-    def verify_name
+    def verify_package_name
       @package_name = match_data[1].to_s.strip
       if @package_name.empty?
         respond("Could not approve. Please, specify the name of the package.")
@@ -29,17 +30,29 @@ module Ropensci
     end
 
     def verify_submission_type
-      @submission_type = read_value_from_body("submission-type").downcase
+      submission_type = read_value_from_body("submission-type").downcase
       if submission_type == "stats"
         statsgrade = read_value_from_body("statsgrade").downcase
-        if ["bronze", "silver", "gold"].include?(stastgrade)
-          labels_to_add << "6/approved-#{stastgrade}"
+        if ["bronze", "silver", "gold"].include?(statsgrade)
+          labels_to_add << "6/approved-#{statsgrade}"
         else
           respond("Please add a grade (bronze/silver/gold) before approval.")
           return false
         end
       end
       true
+    end
+
+    def list_of_reviewers
+      @list_of_reviewers ||= read_value_from_body("reviewers-list").split(",").map(&:strip)
+    end
+
+    def description
+      "Approves a package. This command will close the issue."
+    end
+
+    def example_invocation
+      "@#{@bot_name} approve package-name"
     end
   end
 end
